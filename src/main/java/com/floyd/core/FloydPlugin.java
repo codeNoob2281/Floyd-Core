2eed4cad5d2823c399ba4278b068a66e95e9b8da
huangzf <2281461058@qq.com>
2026-05-12 21:30:27 +0800

    refactor(plugin): Reconstruct the initialization logic of the log

--- src/main/java/com/floyd/core/FloydPlugin.java
+++ src/main/java/com/floyd/core/FloydPlugin.java
@@ -30,12 +30,15 @@ public abstract class FloydPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         setPluginInstance(this);
+        // Early initialize logger
+        ConsoleLogger.initializeFirst(getLogger(), new File(getDataFolder(), LOG_FILE_NAME));
         // Initialize default config
         initConfig();
         // Initialize spring container
         initSpringApplication();
-        // Initialize logger
-        initConsoleLogger();
+        // Initialize logger After spring application initialized
+        PluginSettingsManager pluginSettingsManager = getApplicationContext().getBean(PluginSettingsManager.class);
+        ConsoleLoggerFactory.reloadFromConfig(pluginSettingsManager);
         // Custom plugin initialization logic
         initialize();
         printBanner();
@@ -128,12 +131,6 @@ public abstract class FloydPlugin extends JavaPlugin {
             throw new IllegalStateException("fail to get spring application context, please make sure the plugin is initialized");
         }
         return applicationContext;
-    }
-
-    protected void initConsoleLogger() {
-        ConsoleLogger.initializeFirst(getLogger(), new File(getDataFolder(), LOG_FILE_NAME));
-        PluginSettingsManager pluginSettingsManager = getApplicationContext().getBean(PluginSettingsManager.class);
-        ConsoleLoggerFactory.reloadFromConfig(pluginSettingsManager);
     }
 
     public abstract String getPluginName();
